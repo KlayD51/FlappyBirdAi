2cac740618733053b92436b1b22a9341c306d748
Klay-Michael Dehaney <kdehaney53@gmail.com>
2023-01-18 16:42:51 -0500

    fixed indentation error and added more to classes

--- FlappyBirdGame.py
+++ FlappyBirdGame.py
@@ -4,7 +4,7 @@ import time
 import os
 import random
 
-WIN_WIDTH = 600
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
@@ -36,7 +36,7 @@ class Bird:
     def move(self):
         self.tick_count += 1
 
-        ##bird arc, until next move input
+        #bird arc, until next move input
         d = self.vel*self.tick_count + 1.5*self.tick_count**2
 
         #restrictor or terminal velocity
@@ -56,35 +56,93 @@ class Bird:
             if self.tilt > -90:
                 self.tilt -= self.ROT_VEL
 
+    def draw(self, win):
+        self.img_count += 1
+        #not efficient
+
+        #codes the image of the wings to the bird based on ANIMATION_TIME
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        #when bird dives, removes wing flapping
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        #mystery rotating image code
+        new_rect = rotated_image.get_rect(center = self.img.get_rect(topLeft = (self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)#rotates image
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+    class Pipe:
+        GAP = 200
+        VEL = 5
+
+        def __init__(self, x):
+            self.x = x
+            self.height = 0
+            self.gap = 100
+
+            self.top = 0
+            self.bottom = 0
+            self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+            self.PIPE_BOTTOM = PIPE_IMG
+
+            self.passed = False
+            self.set_height()
+
+        def set_height(self):
+            self.height = random.randrange(50,450)
+            self.top = self.height - self.PIPE_Top.get_height()
+            self.bottom = self.height + self.GAP
+
+        def move(self):
+            self.x -= self.VEL
+
         def draw(self, win):
-            self.img_count += 1
-            #not efficient
+            win.blit(self.PIPE_TOP, (self.x, self.top))
+            win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
-            #codes the image of the wings to the bird based on ANIMATION_TIME
-            if self.img_count < self.ANIMATION_TIME:
-                self.img = self.IMGS[0]
-            elif self.img_count < self.ANIMATION_TIME*2:
-                self.img = self.IMGS[1]
-            elif self.img_count < self.ANIMATION_TIME*3:
-                self.img = self.IMGS[2]
-            elif self.img_count < self.ANIMATION_TIME*4:
-                self.img = self.IMGS[1]
-            elif self.img_count == self.ANIMATION_TIME*4 + 1:
-                self.img = self.IMGS[0]
-                self.img_count = 0
+        
 
-            #when bird dives, removes wing flapping
-            if self.tilt <= -80:
-                self.img = self.IMGS[1]
-                self.img_count = self.ANIMATION_TIME*2
+    def draw_window(win, bird):
+        win.blit(BG_IMG, (0,0))
+        bird.draw(win)
+        pygame.display.update()
+    
+    def main():
+        bird = Bird(200,200)
+        win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        clock = pygame.time.Clock()
 
-            rotated_image = pygame.transform.rotate(self.img, self.tilt)
-            #mystery rotating image code
-            new_rect = rotated_image.get_rect(center = self.img.get_rect(topLeft = (self.x, self.y)).center)
-            win.blit(rotated_image, new_rect.topleft)#rotates image
+        run = True
+        while run:
+            clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            
+            bird.move()
+            draw_window(win, bird)
 
-        def get_mask(self):
-            return pygame.mask.from_surface(self.img)
+        pygame.quit()
+        quit()
+
+    main()
+        
+
 
 
 
